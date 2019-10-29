13e6de503f6300b615861ff616fffe481461be6a
lokeshnk09 <loks.nk@gmail.com>
2019-10-29 07:09:04 -0700

    Anjan Training

--- control_statements/simple_if.py
+++ control_statements/simple_if.py
@@ -4,4 +4,4 @@ if age >= 18:
     print('your eligible for voter id application')
     name = input("enter your full name :")
     DOB = input("enter your date of birth: ")
-print("In-Eligible! Thanks for visiting the voter id Center")
+    print("In-Eligible! Than ks for visiting the voter id Center")
