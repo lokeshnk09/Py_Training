f9200b66060f664bd6dc2c00845012d7207a9efd
Lokeshnk09 <loks.nk@gmail.com>
2020-03-28 18:49:28 -0700

    Office system

--- Aaron_Maxwell/conway.py
+++ Aaron_Maxwell/conway.py
@@ -1,8 +1,16 @@
 import random, time, copy
 
-WIDTH = 60
-HEIGHT = 20
+WIDTH = 5
+HEIGHT = 5
 
 # Create a list of list for the cells:
 
 nextCells = []
+for x in range(WIDTH):
+    column = []
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            column.append('#')
+        else:
+            column.append(' ')
+    nextCells.append(column)
