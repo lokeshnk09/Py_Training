301f11f0185cc5d78c85711c499b4433ce517187
lokeshnk09 <loks.nk@gmail.com>
2020-07-24 00:19:31 -0700

    Final push for regex from Dell Laptop

--- Reg_Ex/re_ex.py
+++ Reg_Ex/re_ex.py
@@ -1,4 +1,4 @@
-
+import os
 import re
 
 text_to_search = '''
@@ -21,13 +21,17 @@ Mrs. Robinson
 Mr. T
 '''
 
+
 sentence = 'Start a sentence and then bring it to an end'
+
+# with open('emails.txt', 'r') as f:
+#     contents = f.read()
 
 # pattern = re.compile(r'\d+[.|-]\d+[-|.]\d+')  # to get all Phone #'s'
 pattern = re.compile(r'M(r|s|rs)\.?\s[A-Z]\w*')  # to get all names
+
 
 matches = pattern.finditer(text_to_search)
 
 for match in matches:
     print(match)
-
