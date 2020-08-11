fbe6008e60e647f7461ed0b37ad42829fa961fab
Wyse_7040 <loks.nk@gmail.com>
2020-08-10 20:08:50 -0700

    Commiting Journeyman from Wyse_7040

--- Reg_Ex/file_lookup.py
+++ Reg_Ex/file_lookup.py
@@ -1,22 +1,29 @@
 import re
 
 
-def lookup(key, file):
-    expression = {'Phones': r'\d+[.|-]\d+[-|.]\d+',
-                  'Names': r'M(r|s|rs)\.?\s[A-Z]\w*'}
-    # d = {k: v for k, v in expression.items()}
-    for k, v in expression.items():
-        if re.match(key, k):
-            with open(file, 'r') as f:
-                contents = f.read()
-            pattern = re.compile(v)
-            matches = pattern.finditer(contents)
-            for match in matches:
-                print(match)
+def decorator_func(func):
+    def lookup(key, file):
+        expression = {'Phones': r'\d+[.|-]\d+[-|.]\d+',
+                      'Names': r'M(r|s|rs)\.?\s[A-Z]\w*'}
+        for k, v in expression.items():
+            if re.match(key, k):
+                with open(file, 'r') as f:
+                    contents = f.read()
+                pattern = re.compile(v)
+                matches = pattern.finditer(contents)
+                for match in matches:
+                    print(match)
+        return func(key, file)
+    return lookup
 
 
+@decorator_func
+def matched_pattern(key, file):
+    print()
 
-c = lookup('Names', 'Entries.txt')
+
+c = matched_pattern('Phones', 'Entries.txt')
+
 
 
 
